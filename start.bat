0320014d9ebbbcde3f5a54220a6f2eed1a447925
Chrisp3251 <chrisp3251@gmail.com>
2026-08-12 11:28:06 -0400

    Enable uvicorn auto reload

--- start.bat
+++ start.bat
@@ -7,5 +7,5 @@ echo Torn Market Scanner V0.1
 echo Dashboard: http://127.0.0.1:8765
 echo.
 start "" powershell.exe -NoProfile -WindowStyle Hidden -Command "Start-Sleep -Seconds 2; Start-Process 'http://127.0.0.1:8765'"
-".venv\Scripts\python.exe" -m uvicorn app:app --host 127.0.0.1 --port 8765
+".venv\Scripts\python.exe" -m uvicorn app:app --host 127.0.0.1 --port 8765 --reload
 pause
